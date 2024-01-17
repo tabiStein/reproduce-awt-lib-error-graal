b999ade420f1bb6d9b4aa816e0c0dbd2314e47c1
Tabi Stein <t.stein@cortical.io>
2024-01-17 19:36:04 +0100

    Small updates (documenting, adding metadata)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,12 +10,18 @@ public class App {
     try {
       File inputPdf = new File("TestPdf.pdf");
 
-      // This line throws "java.lang.UnsatisfiedLinkError: no awt in java.library.path" in the native image
+      /* Usually, this line throws "java.lang.UnsatisfiedLinkError: no awt in java.library.path" in the native image */
       PDDocument document = PDDocument.load(inputPdf);
     } catch (Throwable t) {
-      // ...But, the output is truncated, so we can't see the exception.
-      System.out.println(t.getStackTrace().length);
-      t.printStackTrace(System.out);
+      // ...But, the output is truncated, so we can't see/verify the exception.
+//      System.out.println(t.getMessage());
+      Throwable cause = t.getCause();
+      while (cause != null) {
+        System.out.println(cause);
+        cause = cause.getCause();
+      }
+//      System.out.println(t.getStackTrace().length);
+//      t.printStackTrace(System.out);
     }
     // This never prints
     System.out.println("After catch block");
