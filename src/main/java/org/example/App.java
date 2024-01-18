2594dccfb31b16265597e4dbb5e7d32eac820ede
Tabi Stein <t.stein@cortical.io>
2024-01-18 18:14:08 +0100

    Aha! Finally reproducing the error.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,30 +1,20 @@
 package org.example;
 
 import java.io.File;
+import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
-  // Comments below are WRT the native image.
-  public static void main(String[] args) {
-    try {
-      File inputPdf = new File("TestPdf.pdf");
+  public static void main(String[] args) throws IOException {
+    SpringApplication.run(App.class, args);
+    File inputPdf = new File("TestPdf.pdf");
 
-      /* Usually, this line throws "java.lang.UnsatisfiedLinkError: no awt in java.library.path" in the native image */
-      PDDocument document = PDDocument.load(inputPdf);
-    } catch (Throwable t) {
-      // ...But, the output is truncated, so we can't see/verify the exception.
-//      System.out.println(t.getMessage());
-      Throwable cause = t.getCause();
-      while (cause != null) {
-        System.out.println(cause);
-        cause = cause.getCause();
-      }
-//      System.out.println(t.getStackTrace().length);
-//      t.printStackTrace(System.out);
-    }
-    // This never prints
-    System.out.println("After catch block");
+    /* In the native image, this line throws "java.lang.UnsatisfiedLinkError: no awt in java.library.path" */
+    PDDocument document = PDDocument.load(inputPdf);
   }
 
 }
