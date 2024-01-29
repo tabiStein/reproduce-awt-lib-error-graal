955b3c4db44b19b624ecd430f813521a58265374
Tabi Stein <t.stein@cortical.io>
2024-01-29 16:55:03 +0100

    Clean up file structure to reduce confusion

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +11,7 @@ public class App {
 
   public static void main(String[] args) throws IOException {
     SpringApplication.run(App.class, args);
-    File inputPdf = new File("TestPdf.pdf");
+    InputStream inputPdf = App.class.getResourceAsStream("/TestPdf.pdf");
 
     /* In the native image, this line throws "java.lang.UnsatisfiedLinkError: no awt in java.library.path" */
     PDDocument document = PDDocument.load(inputPdf);
